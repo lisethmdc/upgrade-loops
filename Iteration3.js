b470d3833279e6b082448f192cf993c90513cd8b
lisethmdc <lismc1992@gmail.com>
2021-12-08 20:25:53 +0100

    Iteration#3

---
+++ Iteration3.js
@@ -0,0 +1,13 @@
+const placesToTravel = [
+  "Japon",
+  "Venecia",
+  "Murcia",
+  "Santander",
+  "Filipinas",
+  "Madagascar",
+];
+
+
+for (var destinations of placesToTravel) {
+    console.log(destinations)
+}
